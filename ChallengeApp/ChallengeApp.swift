f5008a6d1fe282b339664ebb03554d5844420a62
Sebastian Villahermosa <svillahermosa16@gmail.com>
2025-05-30 18:07:58 -0300

    Initial project and repo setup

---
+++ ChallengeApp/ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeAppApp.swift
+//  ChallengeApp
+//
+//  Created by VILLAHERMOSA SEBASTIAN on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
